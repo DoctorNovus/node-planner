4d9498453631ec8637b3df56e6ecd5200ff599be
danielwedding <skinner.daniel2004@gmail.com>
2021-05-11 23:45:46 -0600

    Add deletes, swipes, and overall management

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -5,6 +5,8 @@ import boot from "bootstrapcss";
 
 export class TodoItem extends Component {
     fix = new Event("fix");
+    delete = new Event("delete");
+    deleteCount = 0;
 
     render({ item }) {
         let { title, date, info, forClass, count, active } = item;
@@ -23,11 +25,20 @@ export class TodoItem extends Component {
     }
 
     mount() {
-        if (!this.props.item.host)
+        if (!this.props.item.host) {
             this.root.querySelector("a").addEventListener("click", (e) => {
                 this.fix.item = this.props.item;
                 this.dispatchEvent(this.fix);
             });
+
+            let that = this;
+
+            this.root.querySelector("a").addEventListener('touchstart', (e) => this.handleTouchStart(that, e), false);
+            this.root.querySelector("a").addEventListener('touchmove', (e) => this.handleTouchMove(that, e), false);
+
+            this.xDown = null;
+            this.yDown = null;
+        }
     }
 
     howManyDays(date) {
@@ -39,6 +50,53 @@ export class TodoItem extends Component {
 
         return date.getDay() - day + 2;
     }
+
+    getTouches(evt) {
+        return evt.touches ||             // browser API
+            evt.originalEvent.touches; // jQuery
+    }
+
+    handleTouchStart(that, evt) {
+        const firstTouch = that.getTouches(evt)[0];
+        that.xDown = firstTouch.clientX;
+        that.yDown = firstTouch.clientY;
+    };
+
+    handleTouchMove(that, evt) {
+        if (!that.xDown || !that.yDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = that.xDown - xUp;
+        var yDiff = that.yDown - yUp;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+            if (xDiff > 0) {
+                this.deleteCount = 0;
+                this.root.querySelector("a").style.backgroundColor = "white";
+            } else {
+                this.deleteCount++;
+                this.root.querySelector("a").style.backgroundColor = "red";
+                if (this.deleteCount == 2) {
+                    this.delete.item = this.props.item;
+                    this.dispatchEvent(this.delete);
+                    this.deleteCount = 0;
+                }
+            }
+        } else {
+            if (yDiff > 0) {
+                /* up swipe */
+            } else {
+                /* down swipe */
+            }
+        }
+        /* reset values */
+        that.xDown = null;
+        that.yDown = null;
+    };
 }
 
 Component.create({ name: "todo-item", styles: [style, boot] }, TodoItem);
--- src/components/todo-manager/todo-manager.js
+++ src/components/todo-manager/todo-manager.js
@@ -16,24 +16,76 @@ export class TodoManager extends Component {
         return html`
             <view-today total=${this.inactives.length + this.actives.length} todo=${this.actives.length} />
             <div class="list-group">
-                ${this.actives.sort((a, b) => a.title > b.title).map((task, index) => html`<todo-item index=${index} item=${task} onfix=${(e) => this.swapItem(e.target, e.item)} />`)}
+                ${this.actives.map((task, index) => html`<todo-item index=${index} item=${task} onfix=${(e)=>
+                    this.swapItem(e.target, e.item)} ondelete=${(e) =>
+                    this.deleteTask(e.target, e.item)} />`)}
             </div>
             <br>
             <div class="list-group">
-                ${this.inactives.sort((a, b) => a.title > b.title).map((task, index) => html`<todo-item index=${index} item=${task} onfix=${(e) => this.swapItem(e.target, e.item)} />`)}
+                ${this.inactives.map((task, index) => html`<todo-item index=${index} item=${task} onfix=${(e)=>
+                        this.swapItem(e.target, e.item)} ondelete=${(e) =>
+                            this.deleteTask(e.target, e.item)} />`)}
             </div>
+            <div id="addBox" class="invisible">
+                <form onsubmit=${(e)=> this.createTask(e)}>
+                    <label for="title">Task Title <span onclick=${()=> this.root.querySelector("#addBox").classList =
+                            "invisible"}>X</span></label>
+                    <input type="text" id="title" name="title" placeholder="Insert title here.." />
+                    <label for="due">Due Date</label>
+                    <input type="date" value="${new Date().toDateInputValue()}" id="due" name="due" />
+                    <label for="info">Details</label>
+                    <input id="info" name="info" type="text" placeholder="Insert task info here.." />
+                    <label for="category">Category</label>
+                    <input id="category" name="category" type="text" placeholder="Insert category here.." />
+                    <button type="submit">Create</button>
+                </form>
+            </div>
+            <footer>
+                <h1 onclick=${()=> this.root.querySelector("#addBox").classList = ""}>+</h1>
+            </footer>
         `;
+    }
+
+    createTask(e) {
+        e.preventDefault();
+        let objs = e.path[0].children;
+        let obj = {
+            id: (this.actives.length + this.inactives.length),
+            title: objs.title.value,
+            date: new Date(objs.due.value),
+            info: objs.info.value,
+            category: objs.category.value
+        };
+
+        let newArr = this.actives;
+        newArr.push(obj);
+        this.actives = newArr;
+
+        this.root.querySelector("#addBox").classList = "invisible";
+    }
+
+    deleteTask(object, item) {
+        let ite = this.actives.find(ite => ite.id == item.id);
+        let ite2 = this.inactives.find(ite => ite.id == item.id);
+
+        if (ite) {
+            this.actives.splice(this.actives.findIndex(ite => ite.id == item.id), 1);
+        }
+
+        if (ite2) {
+            this.inactives.splice(this.actives.findIndex(ite => ite.id == item.id), 1);
+        }
     }
 
     mount() {
         this.actives = [
             { id: 0, title: "Tasks to do", date: new Date(), active: true, done: false, host: true },
-            { id: 1, title: "9.4 Quiz", date: new Date(), info: "Quiz is found in Teams.", forClass: "Chemistry", done: false },
-            { id: 2, title: "Chapter 15 Quiz", date: new Date(), info: "Quiz found in college portal.", forClass: "Math 122", done: false }
+            { id: 2, title: "9.4 Quiz", date: new Date(), info: "Quiz is found in Teams.", category: "Chemistry", done: false },
+            { id: 3, title: "Chapter 15 Quiz", date: new Date(), info: "Quiz found in college portal.", category: "Math 122", done: false }
         ];
 
         this.inactives = [
-            { id: 0, title: "Tasks finished", date: new Date(), active: true, done: true, host: true },
+            { id: 1, title: "Tasks finished", date: new Date(), active: true, done: true, host: true },
         ]
     }
 
@@ -47,15 +99,21 @@ export class TodoManager extends Component {
             newItem.done = !newItem.done;
             let newArr = this.inactives;
             newArr.push(newItem[0]);
-            this.inactives = newArr.sort((a, b) => a.title > b.title);
+            this.inactives = newArr.sort((a, b) => a.date.getTime() < b.date.getTime());
         } else if (ite2) {
             let newItem = this.inactives.splice(this.actives.findIndex(ite => ite.id == item.id), 1);
             newItem.done = !newItem.done;
             let newArr = this.actives;
             newArr.push(newItem[0]);
-            this.actives = newArr.sort((a, b) => a.title > b.title);
+            this.actives = newArr.sort((a, b) => a.date.getTime() < b.date.getTime());
         }
     }
 }
+
+Date.prototype.toDateInputValue = (function () {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0, 10);
+});
 
 Component.create({ name: "todo-manager", styles: [style, boot] }, TodoManager);
